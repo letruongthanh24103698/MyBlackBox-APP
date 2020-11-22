f9079002b555d64bca0bfd50e2b20768ff72acb0
letruongthanh24103698 <letruongthanh3698@gmail.com>
2020-11-22 14:03:21 +0700

    Release v1.0.0

---
+++ MyBlackBox/Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace MyBlackBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnGetFile = new System.Windows.Forms.Button();
+            this.lblSize = new System.Windows.Forms.Label();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnGetName = new System.Windows.Forms.Button();
+            this.lbxFileName = new System.Windows.Forms.ListBox();
+            this.tbxIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.btnOpenPort = new System.Windows.Forms.Button();
+            this.cbxBaudrate = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbxComPort = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbxLog = new System.Windows.Forms.RichTextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.tbxFile = new System.Windows.Forms.TextBox();
+            this.lblFile = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.tbxIP);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(343, 420);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "DOWNLOAD";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.btnGetFile);
+            this.groupBox3.Controls.Add(this.lblSize);
+            this.groupBox3.Controls.Add(this.lblFileName);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Location = new System.Drawing.Point(14, 311);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(308, 101);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Get File";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(163, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = " DELETE FILE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnGetFile
+            // 
+            this.btnGetFile.Location = new System.Drawing.Point(35, 72);
+            this.btnGetFile.Name = "btnGetFile";
+            this.btnGetFile.Size = new System.Drawing.Size(109, 23);
+            this.btnGetFile.TabIndex = 2;
+            this.btnGetFile.Text = "GET FILE";
+            this.btnGetFile.UseVisualStyleBackColor = true;
+            this.btnGetFile.Click += new System.EventHandler(this.btnGetFile_Click);
+            // 
+            // lblSize
+            // 
+            this.lblSize.AutoSize = true;
+            this.lblSize.Location = new System.Drawing.Point(72, 48);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(35, 13);
+            this.lblSize.TabIndex = 3;
+            this.lblSize.Text = "NULL";
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(72, 26);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(35, 13);
+            this.lblFileName.TabIndex = 2;
+            this.lblFileName.Text = "NULL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Size :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "File Name :";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnGetName);
+            this.groupBox2.Controls.Add(this.lbxFileName);
+            this.groupBox2.Location = new System.Drawing.Point(13, 44);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(309, 252);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "File Name";
+            // 
+            // btnGetName
+            // 
+            this.btnGetName.Location = new System.Drawing.Point(98, 18);
+            this.btnGetName.Name = "btnGetName";
+            this.btnGetName.Size = new System.Drawing.Size(109, 23);
+            this.btnGetName.TabIndex = 1;
+            this.btnGetName.Text = "GET FILE NAME";
+            this.btnGetName.UseVisualStyleBackColor = true;
+            this.btnGetName.Click += new System.EventHandler(this.btnGetName_Click);
+            // 
+            // lbxFileName
+            // 
+            this.lbxFileName.FormattingEnabled = true;
+            this.lbxFileName.Location = new System.Drawing.Point(6, 47);
+            this.lbxFileName.Name = "lbxFileName";
+            this.lbxFileName.Size = new System.Drawing.Size(297, 199);
+            this.lbxFileName.TabIndex = 0;
+            this.lbxFileName.SelectedIndexChanged += new System.EventHandler(this.lbxFileName_SelectedIndexChanged);
+            // 
+            // tbxIP
+            // 
+            this.tbxIP.Location = new System.Drawing.Point(75, 18);
+            this.tbxIP.Name = "tbxIP";
+            this.tbxIP.Size = new System.Drawing.Size(247, 20);
+            this.tbxIP.TabIndex = 1;
+            this.tbxIP.Text = "http://192.168.4.115";
+            this.tbxIP.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP Address";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.btnOpenPort);
+            this.groupBox4.Controls.Add(this.cbxBaudrate);
+            this.groupBox4.Controls.Add(this.label6);
+            this.groupBox4.Controls.Add(this.cbxComPort);
+            this.groupBox4.Controls.Add(this.label5);
+            this.groupBox4.Controls.Add(this.tbxLog);
+            this.groupBox4.Controls.Add(this.btnStart);
+            this.groupBox4.Controls.Add(this.btnBrowser);
+            this.groupBox4.Controls.Add(this.tbxFile);
+            this.groupBox4.Controls.Add(this.lblFile);
+            this.groupBox4.Location = new System.Drawing.Point(370, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(417, 420);
+            this.groupBox4.TabIndex = 1;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "VISUALIZE DATA";
+            // 
+            // btnOpenPort
+            // 
+            this.btnOpenPort.Location = new System.Drawing.Point(333, 42);
+            this.btnOpenPort.Name = "btnOpenPort";
+            this.btnOpenPort.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenPort.TabIndex = 12;
+            this.btnOpenPort.Text = "Open";
+            this.btnOpenPort.UseVisualStyleBackColor = true;
+            this.btnOpenPort.Click += new System.EventHandler(this.btnOpenPort_Click);
+            // 
+            // cbxBaudrate
+            // 
+            this.cbxBaudrate.FormattingEnabled = true;
+            this.cbxBaudrate.Location = new System.Drawing.Point(235, 44);
+            this.cbxBaudrate.Name = "cbxBaudrate";
+            this.cbxBaudrate.Size = new System.Drawing.Size(92, 21);
+            this.cbxBaudrate.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(179, 47);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Baudrate";
+            // 
+            // cbxComPort
+            // 
+            this.cbxComPort.FormattingEnabled = true;
+            this.cbxComPort.Location = new System.Drawing.Point(80, 44);
+            this.cbxComPort.Name = "cbxComPort";
+            this.cbxComPort.Size = new System.Drawing.Size(93, 21);
+            this.cbxComPort.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Com Port";
+            // 
+            // tbxLog
+            // 
+            this.tbxLog.Location = new System.Drawing.Point(5, 71);
+            this.tbxLog.Name = "tbxLog";
+            this.tbxLog.ReadOnly = true;
+            this.tbxLog.Size = new System.Drawing.Size(402, 308);
+            this.tbxLog.TabIndex = 7;
+            this.tbxLog.Text = "";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(190, 391);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.Location = new System.Drawing.Point(333, 16);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowser.TabIndex = 5;
+            this.btnBrowser.Text = "Browser";
+            this.btnBrowser.UseVisualStyleBackColor = true;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
+            // 
+            // tbxFile
+            // 
+            this.tbxFile.Location = new System.Drawing.Point(80, 18);
+            this.tbxFile.Name = "tbxFile";
+            this.tbxFile.Size = new System.Drawing.Size(247, 20);
+            this.tbxFile.TabIndex = 4;
+            // 
+            // lblFile
+            // 
+            this.lblFile.AutoSize = true;
+            this.lblFile.Location = new System.Drawing.Point(6, 21);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(23, 13);
+            this.lblFile.TabIndex = 4;
+            this.lblFile.Text = "File";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 436);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbxIP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox lbxFileName;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnGetFile;
+        private System.Windows.Forms.Label lblSize;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGetName;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.RichTextBox tbxLog;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnBrowser;
+        private System.Windows.Forms.TextBox tbxFile;
+        private System.Windows.Forms.Label lblFile;
+        private System.Windows.Forms.ComboBox cbxBaudrate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbxComPort;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnOpenPort;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MyBlackBox/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MyBlackBox
+{
+    public enum PortStatusEnum
+    {
+        CLOSED,
+        OPENED
+    }
+
+    public partial class Form1 : Form
+    {
+        private MySerialPort com = new MySerialPort();
+
+        private string IP_S;
+        private string GetNames_Suffix_S = "/GetNames";
+        private string GetSize_Suffix_S = "/GetSize";
+        private string GetFile_Suffix_S = "/GetFile";
+        private string DelFile_Suffix_S = "/DelFile";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] BaudRate = { "1200", "2400", "4800", "9600", "19200", "38400", "57600", "115200" };
+            cbxBaudrate.Items.AddRange(BaudRate);
+            cbxBaudrate.SelectedIndex = 3;//9600
+            cbxComPort.DataSource = SerialPort.GetPortNames();
+        }
+
+        private void btnOpenPort_Click(object sender, EventArgs e)
+        {
+            if (com.Status == PortStatusEnum.CLOSED)
+            {
+                com.Port.PortName = cbxComPort.Text;
+                com.Port.BaudRate = Convert.ToInt32(cbxBaudrate.Text);
+                do
+                {
+                    com.Port.Open();
+                } while (!com.Port.IsOpen);
+                btnOpenPort.Text = "Close";
+            }
+            else
+            {
+                do
+                {
+                    com.Port.Close();
+                } while (com.Port.IsOpen);
+                btnOpenPort.Text = "Open";
+            }
+        }
+
+        private void btnGetName_Click(object sender, EventArgs e)
+        {
+            lbxFileName.Items.Clear();
+            string uri = tbxIP.Text + GetNames_Suffix_S;
+            WebClient client = new WebClient();
+            try
+            {
+                string result = client.DownloadString(uri);
+                int pos = 0;
+                int prepos = 0;
+                result += ',';
+                do
+                {
+                    pos = result.IndexOf(',', prepos);
+                    if (pos == -1)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        lbxFileName.Items.Add(result.Substring(prepos, pos - prepos));
+                    }
+                    prepos = pos + 1;
+                } while (true);
+                MessageBox.Show("Get Name Done");
+            }
+            catch
+            {
+                MessageBox.Show("Get Name Failed");
+            }
+        }
+
+        private bool Ping()
+        {
+            string uri = tbxIP.Text;
+            int port = 80;
+            try
+            {
+                using (var client = new TcpClient(uri, port))
+                    return true;
+            }
+            catch (SocketException ex)
+            {
+                return false;
+            }
+        }
+
+        private int GetSize(string name)
+        {
+            string uri = tbxIP.Text + GetSize_Suffix_S;
+
+            WebClient client = new WebClient();
+            client.QueryString.Add("name", name);
+
+            try
+            {
+                string result = client.DownloadString(uri);
+                return Convert.ToInt32(result);
+            }
+            catch
+            {
+                //TODO
+                return -1;
+            }
+        }
+
+        private void lbxFileName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string[] tmp = lbxFileName.GetItemText(lbxFileName.SelectedItem).Split('|');
+            if (lbxFileName.SelectedIndex == -1)
+            {
+                lblFileName.Text = "NULL";
+                lblSize.Text = "NULL";
+            }
+            else
+            {
+                lblFileName.Text = tmp[0];
+                lblSize.Text = tmp[1];
+            }
+        }
+
+        private string GetFile(string name, int part, int partsize)
+        {
+            string uri = tbxIP.Text + GetFile_Suffix_S;
+
+            WebClient client = new WebClient();
+            client.QueryString.Add("name", name);
+            client.QueryString.Add("part", part.ToString());
+            client.QueryString.Add("partsize", partsize.ToString());
+
+            try
+            {
+                string result = client.DownloadString(uri);
+                return result;
+            }
+            catch
+            {
+                //TODO
+                return "";
+            }
+        }
+
+        private void btnGetFile_Click(object sender, EventArgs e)
+        {
+            string name = lblFileName.Text.Substring(1);
+            int size = GetSize(name);
+
+            int size_per_part = 10000;
+
+            int part = (int)(Math.Ceiling((double)size / (double)size_per_part));
+
+            if (!System.IO.File.Exists(name))
+            {
+                File.Create(name).Close();
+                using (StreamWriter sw = File.AppendText(name))
+                {
+
+                    string result;
+                    for (int i = 0; i < part; i++)
+                    {
+                        btnGetFile.Text = "GET FILE " + ((int)((double)i / (double)part * 100.0)).ToString() + "%";
+                        result = GetFile(name, i, size_per_part);
+                        sw.Write(result);
+                    }
+                }
+                MessageBox.Show("Get File Done");
+                btnGetFile.Text = "GET FILE";
+            }
+            
+        }
+
+        private void btnBrowser_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog
+            {
+                Title = "Browse Text File",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "txt",
+                Filter = "txt files (*.txt)|*.txt",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                tbxFile.Text = openFile.FileName;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!com.Port.IsOpen)
+            {
+                MessageBox.Show("Open Com Port First!");
+                return;
+            }
+            tbxLog.Text = "";
+            string fileurl = tbxFile.Text;
+            
+            using (StreamReader sr= File.OpenText(fileurl))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string sentence = sr.ReadLine();
+                    com.Port.WriteLine(sentence);
+                    tbxLog.AppendText(sentence);
+                    tbxLog.ScrollToCaret();
+                    System.Threading.Thread.Sleep(20);
+                }
+            }
+            MessageBox.Show("Stream Done");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string uri = tbxIP.Text + DelFile_Suffix_S;
+
+            try
+            {
+                WebClient client = new WebClient();
+                string name = lblFileName.Text.Substring(1).Split('|')[0];
+                client.QueryString.Add("name", name);
+                client.DownloadString(uri);
+
+                int index = lbxFileName.SelectedIndex;
+                lbxFileName.Items.RemoveAt(index);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Delete Failed!");
+            }
+        }
+    }
+
+    public class MySerialPort
+    {
+        public SerialPort Port = new SerialPort();
+        public PortStatusEnum Status = PortStatusEnum.CLOSED;
+    }
+}
